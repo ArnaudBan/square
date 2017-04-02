078e283bd062519bacb9c4d091c46b4bd34f926b
ArnaudBan <abanvillet@studio-goliath.com>
2017-04-02 10:46:35 +0200

    Add archive header

--- index.php
+++ index.php
@@ -7,6 +7,21 @@ get_header();
 
 if( have_posts() ){
 
+
+    $archive_tite = get_the_archive_title();
+    $archive_description =  get_the_archive_description();
+    ?>
+    <div class="archive-page-header">
+
+        <div class="archive-title-wrapper">
+            <h1 class="archive-title"><?php echo $archive_tite; ?></h1>
+        </div>
+        <div class="archive-description-wrapper">
+            <div class="archive-description"><?php echo $archive_description; ?></div>
+        </div>
+    </div>
+    <?php
+
     echo '<div class="post-grid">';
 
     while ( have_posts() ){
