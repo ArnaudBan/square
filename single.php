7e9d5a8ccd18695fcf63b95ad94fb8e0fb7d1fda
ArnaudBan <abanvillet@studio-goliath.com>
2017-05-13 14:37:58 +0200

    Add related post/project

--- single.php
+++ single.php
@@ -47,8 +47,71 @@ the_post();
 
     </div>
 
+    <?php
+    $post_type = get_post_type();
+    get_sidebar( $post_type );
 
-    <?php get_sidebar( get_post_type() ); ?>
+    $archive_class = 'post-grid';
+    $nb_related_post = 3;
+    $related_post_title = __('Related posts', 'square');
+
+    if( $post_type == 'project' ){
+
+        $archive_class = 'project-grid';
+        $nb_related_post = 2;
+        $related_post_title = __('Related projects', 'square');
+    }
+
+    global $post;
+    $topics = get_the_terms( $post, 'topic' );
+    $topics_id = wp_list_pluck( $topics, 'term_id');
+
+    $related_post_args = array(
+        'post_type'         => $post_type,
+        'posts_per_page'    => $nb_related_post,
+        'post__not_in'      => array( get_the_ID() ),
+        'tax_query'         => array(
+            array(
+                array(
+                    'taxonomy' => 'topic',
+                    'field'    => 'id',
+                    'terms'    => $topics_id,
+                ),
+            )
+        ),
+        'no_found_rows' => true
+    );
+
+    $related_post_query = new WP_Query( $related_post_args );
+
+    if( $related_post_query->have_posts() ){
+
+        ?>
+
+        <div class="related-post">
+
+            <h2 class="related-post-title"><?php echo $related_post_title; ?></h2>
+
+            <div class="<?php echo $archive_class; ?>">
+
+                <?php
+
+                while ( $related_post_query->have_posts() ){
+                    $related_post_query->the_post();
+
+                    get_template_part('parts/content', get_post_type() );
+                }
+
+
+                wp_reset_query();
+                ?>
+            </div>
+
+        </div>
+
+        <?php
+    }
+    ?>
 </div>
 
 <?php
